a5cd166b4b32a764ed6b0ed2a04676848c128d8f
Daniel Smirnov <rock4777@users.noreply.github.com>
2023-02-24 10:41:32 -0500

    Add requirements.txt and ps script

---
+++ txt_end_script.ps1
@@ -0,0 +1,19 @@
+﻿$files = Get-ChildItem "" #directory where you want to change file extension
+
+foreach ($f in $files){
+    $outfile = $f.FullName 
+    $fileName = $f.Name
+
+    if ($fileName -Like "*.txt")
+    {
+          #do nothing
+
+    } 
+    else
+    {
+        $newName = $fileName + ".txt"        
+        Rename-Item -Path $outfile -NewName $newName
+
+    }      
+
+} # end of for loop
